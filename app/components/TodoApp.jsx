cefdaff066359c1ea07c2fb0aee2a490e1bb5b3c
Tetyana <tatankavolkova@gmail.com>
2017-10-09 17:20:29 -0400

    Add toogle on checkbox.

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -13,19 +13,23 @@ var TodoApp = React.createClass({
 			todos: [
 				{
 					id: uuid(),
-					text: 'Walk the dog'
+					text: 'Walk the dog',
+					completed: false
 				},
 				{
 					id: uuid(),
-					text: 'Clean the yard'
+					text: 'Clean the yard',
+					completed: true
 				},
 				{
 					id: uuid(),
-					text: 'Finish the article'
+					text: 'Finish the article',
+					completed: true
 				},
 				{
 					id: uuid(),
-					text: 'Find job'
+					text: 'Find job',
+					completed: false
 				}
 			]
 		};
@@ -36,10 +40,22 @@ var TodoApp = React.createClass({
 				...this.state.todos,
 				{
 					id: uuid(),
-					text: text
+					text: text,
+					completed: false
 				}
 			]
 		});
+	},
+	handleToggle: function(id) {
+		var updatedTodos = this.state.todos.map((todo) => {
+			if(todo.id === id) {
+				todo.completed = !todo.completed;
+			}
+
+			return todo;
+		});
+
+		this.setState({todos: updatedTodos});
 	},
 	handleSearch: function(showComplited, searchText) {
 		this.setState({
@@ -55,7 +71,7 @@ var TodoApp = React.createClass({
 		return(
 			<div>
 				<TodoSearch onSearch = {this.handleSearch} />
-				<TodoList todos={todos} />
+				<TodoList todos={todos} onToggle={this.handleToggle} />
 				<TodoListForm handleAddTodo={this.handleAddTodo} />
 			</div>
 		)
